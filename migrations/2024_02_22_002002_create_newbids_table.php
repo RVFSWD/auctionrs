4faa5db7bff912d5098d86721496c98068b3ab23
RVFSWD <146232937+RVFSWD@users.noreply.github.com>
2024-03-02 14:34:24 +0800

    Add files via upload

---
+++ migrations/2024_02_22_002002_create_newbids_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('newbids', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('user_name');
+            $table->longText('bid');
+            $table->foreignId('user_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('newbids');
+    }
+};
